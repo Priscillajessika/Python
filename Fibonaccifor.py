71679d668691c9944312c6c88bfd6f5f638336a0
ROSA <priscila.rosa@vidanova.local>
2022-05-28 19:15:26 -0300

    Calculando o Fibonacci com For

---
+++ Fibonaccifor.py
@@ -0,0 +1,17 @@
+quantidade = int(input("Digite um numero inteiro para o calculo do FIBONACCI: "))
+
+numero1 = 0
+numero2 = 1
+
+#imprimindo as variaveis iniciais
+print(numero1)
+print(numero2)
+
+#calculando o fibonacci
+for x in range (quantidade): 
+    numero3 = numero1 + numero2
+    print(numero3)
+    numero1 = numero2
+    numero2 = numero3
+    
+
